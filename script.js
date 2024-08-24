3c95d7dc7b4c91c06e411f5f660f251e4022b58c
gangaramA <150507677+gangaramA@users.noreply.github.com>
2024-08-24 09:42:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+  const form = document.getElementById("form1");
+  const formData = new FormData(form);
 
+  const firstName = formData.get("fname");
+  const lastName = formData.get("lname");
+
+  alert(`${firstName} ${lastName}`);
+
+  return false; // Prevent default form submission behavior
 }
